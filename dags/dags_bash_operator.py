d9a11e997ff586e0369845d13f79eadcf14c738f
yangsungsoo <ysotgood@gmail.com>
2024-08-07 00:13:00 +0900

    오타 수정 hostnamme -> hostname

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -23,8 +23,7 @@ with DAG(
 
     bash_t2 = BashOperator(
                 task_id="bash_t2",
-                bash_command="echo $HOSTNAMME",
+                bash_command="echo $HOSTNAME",
     )
 
     bash_t1 >> bash_t2
-    
